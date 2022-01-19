719f9fd166c9a40cfdfdd2b401a7b576c89764a5
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2022-01-19 22:21:52 +0300

    Добавьте файлы проекта.

---
+++ networkProject/Network/NetworkClient.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace networkProject.Network
+{
+  public  class NetworkClient
+    {
+        public static readonly Socket ClientSocket = new Socket
+         (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public const int PORT = 27001;
+
+    
+
+        public static void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+
+        public static void RequestLoop()
+        {
+            Console.WriteLine(@"<Type ""exit"" to properly disconnect client>");
+
+            var sender = Task.Run(() =>
+            {
+                while (true)
+                {
+                    SendRequest();
+                }
+            });
+            var receiver = Task.Run(() =>
+            {
+
+                while (true)
+                {
+                    ReceiveResponse();
+
+                }
+            });
+
+            Task.WaitAll(sender, receiver);
+        }
+
+        /// <summary>
+        /// Close socket and exit program.
+        /// </summary>
+        public static void Exit()
+        {
+            SendString("exit"); // Tell the server we are exiting
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        public  static void SendRequest()
+        {
+            Console.Write("Send a request: ");
+            string request = Console.ReadLine();
+            SendString(request);
+
+            if (request.ToLower() == "exit")
+            {
+                Exit();
+            }
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding.
+        /// </summary>
+        public static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        public static void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine(text);
+        }
+
+    }
+}
---
+++ networkProject/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using networkProject.Command;
+using networkProject.Network;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace networkProject.ViewModel
+{
+    class MainViewModel:BaseViewModel
+    {
+       
+        public RelayCommand CloseCommand { get; set; }
+        public RelayCommand VoiceCommand { get; set; }
+        public RelayCommand PhotoCommand { get; set; }
+        public RelayCommand VideoCommand { get; set; }
+        public RelayCommand SendCommand { get; set; }
+      
+        public DispatcherTimer Timer { get; set; } = new DispatcherTimer();
+        
+        public MainViewModel(MainWindow mainwindow)
+        {
+            CloseCommand = new RelayCommand((s) =>
+            {
+                mainwindow.window.Close();
+            });
+            VideoCommand = new RelayCommand((s) =>
+            {
+            });
+            PhotoCommand = new RelayCommand((s) =>
+            {
+            });
+            VoiceCommand = new RelayCommand((s) =>
+            {
+            });
+            SendCommand = new RelayCommand((s) =>
+            {
+                Task.Run(() =>
+                {
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (!NetworkClient.ClientSocket.Connected)
+                        {
+                            NetworkClient.ConnectToServer();
+                        }
+                        byte[] buffer = Encoding.ASCII.GetBytes(mainwindow .messtxtb .Text );
+                        NetworkClient.ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+                        mainwindow.list.Items.Add(mainwindow.messtxtb.Text);
+                        NetworkClient.SendString(mainwindow.messtxtb.Text);
+                    });
+                });
+            });
+
+        }
+    }
+}
